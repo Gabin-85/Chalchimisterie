a5f6d6f59880212c746a427a48def7f155b60d2b
Gabin-85 <quagligabin.praud@gmail.com>
2024-04-16 22:05:57 +0200

    Dumb vector mouvement

--- src/player.py
+++ src/player.py
@@ -1,8 +1,9 @@
 import pygame
 from utils.consoleHandler import *
-from time import wait
+import math
 
-velocity = pygame.Vector2(0, 0)
+DIAGONALY = math.sqrt(2)/2
+
 class Player(pygame.sprite.Sprite):
 
     def __init__(self):
@@ -14,6 +15,10 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.last_position = [None, None]
         self.feet = pygame.Rect(0, 0, 0, 0)
+        self.glide = 0.995
+        self.allow_move = True
+        self.velocity = pygame.Vector2(0, 0)
+        self.acceleration = pygame.Vector2(0, 0)
 
         # After this you can add varaibles for the player like inventory and others stuffs :
 
@@ -24,27 +29,49 @@ class Player(pygame.sprite.Sprite):
         return image
 
     def player_move(self):
-        global velocity;
         pressed = pygame.key.get_pressed()
 
         if pressed[pygame.K_LSHIFT] or pressed[pygame.K_RSHIFT]:
-            speed = 0.8
+            self.acceleration = [0.8, 0.8]
         else:
-            speed = 0.5
+            self.acceleration = [0.5, 0.5]
 
-        if pressed[pygame.K_UP]:
-            velocity = [velocity[0], velocity[1]-speed]
-        elif pressed[pygame.K_DOWN]:
-            velocity = [velocity[0], velocity[1]+speed]
-        if pressed[pygame.K_LEFT]:
-            velocity = [velocity[0]-speed, velocity[1]]
-        elif pressed[pygame.K_RIGHT]:
-            velocity = [velocity[0]+speed, velocity[1]]
-        # roulade
+        if pressed[pygame.K_UP] and not pressed[pygame.K_DOWN]:
+            if pressed[pygame.K_LEFT] and not pressed[pygame.K_RIGHT]:
+                self.acceleration = [-self.acceleration[0]*DIAGONALY, -self.acceleration[1]*DIAGONALY]
+            elif pressed[pygame.K_RIGHT] and not pressed[pygame.K_LEFT]:
+                self.acceleration = [self.acceleration[0]*DIAGONALY, -self.acceleration[1]*DIAGONALY]
+            else:
+                self.acceleration = [0, -self.acceleration[1]]
+        elif pressed[pygame.K_DOWN] and not pressed[pygame.K_UP]:
+            if pressed[pygame.K_LEFT] and not pressed[pygame.K_RIGHT]:
+                self.acceleration = [-self.acceleration[0]*DIAGONALY, self.acceleration[1]*DIAGONALY]
+            elif pressed[pygame.K_RIGHT] and not pressed[pygame.K_LEFT]:
+                self.acceleration = [self.acceleration[0]*DIAGONALY, self.acceleration[1]*DIAGONALY]
+            else:
+                self.acceleration = [0, self.acceleration[1]]
+        else:
+            if pressed[pygame.K_LEFT] and not pressed[pygame.K_RIGHT]:
+                self.acceleration = [-self.acceleration[0], 0]
+            elif pressed[pygame.K_RIGHT] and not pressed[pygame.K_LEFT]:
+                self.acceleration = [self.acceleration[0], 0]
+            else:
+                self.acceleration = [0, 0]
         if pressed[pygame.K_SPACE]:
-            self.position = [velocity[1]*3, velocity[1]*3]
-            wait(0.3)
-            self.position = [0, 0]
+            self.acceleration = self.acceleration[0]*500, self.acceleration[1]*500
+            if self.allow_move: 
+                self.velocity = self.velocity[0]*self.glide + self.acceleration[0]*(-self.glide+1), self.velocity[1]*self.glide + self.acceleration[1]*(-self.glide+1)
+            self.allow_move = False
+
+        if self.allow_move:
+            self.velocity = self.velocity[0]*self.glide + self.acceleration[0]*(-self.glide+1), self.velocity[1]*self.glide + self.acceleration[1]*(-self.glide+1)
+        else:
+            self.velocity = self.velocity[0]*self.glide, self.velocity[1]*self.glide
+            if round(self.velocity[0], 1) == 0 and round(self.velocity[1], 1) == 0:
+                self.allow_move = True
+
+
+        self.position = [self.position[0]+self.velocity[0], self.position[1]+self.velocity[1]]
         self.feet = pygame.Rect(self.position[0], 28+self.position[1], 22, 18)
 
     def update(self):
