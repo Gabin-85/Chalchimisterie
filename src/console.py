e508a25417694b6946e0e74a34c132c4915c87db
Gabin-85 <quagligabin.praud@gmail.com>
2025-01-22 22:42:31 +0100

    File.py migration and Console.py rewrite

---
+++ src/console.py
@@ -0,0 +1,55 @@
+from args import  ext, formating, consoleLevel
+from datetime import datetime
+
+broadcast_logfiles = []
+logfiles = []
+
+def add_logfile(filepath:str, broadcast:bool=False) -> None:
+    try:
+        with open(f"{filepath}{ext.log}", "w") as file:
+            file.write("")
+        
+        if broadcast == True:
+            broadcast_logfiles.append(open(f"{filepath}{ext.log}", "a"))
+        else:
+            logfiles.append(open(f"{filepath}{ext.log}", "a"))
+        trace(f"Log file '{filepath}{ext.log}' selected.")
+    except FileNotFoundError:
+        error(f"Directory doesn't exist. Logger is disabled.")
+
+def print_logfile(level:str, msg:str, logfiles:list[str]) -> None:
+    if level not in consoleLevel:
+        return
+    
+    for logfile in logfiles:
+        logfile.write(f"({datetime.now().strftime('%H:%M:%S.%f')[:-3]}) [{level.upper()}]: {msg}\n")
+
+def print_console(level:str, msg:str, formatings:list[formating]) -> None:
+    if level not in consoleLevel:
+        return
+    
+    print(f"{''.join(form for form in formatings)}[{level.upper()}]: {msg}{formating.clear}")
+
+def fatal(msg:str, logfiles=broadcast_logfiles) -> None:
+    print_console("fatal", msg, [formating.red,formating.inverted,formating.bold])
+    print_logfile("fatal", msg, logfiles)
+
+def error(msg:str, logfiles=broadcast_logfiles) -> None:
+    print_console("error", msg, [formating.red,formating.bold])
+    print_logfile("error", msg, logfiles)
+
+def warn(msg:str, logfiles=broadcast_logfiles) -> None:
+    print_console("warn", msg, [formating.yellow])
+    print_logfile("warn", msg, logfiles)
+
+def info(msg:str, logfiles=broadcast_logfiles) -> None:
+    print_console("info", msg, [formating.green])
+    print_logfile("info", msg, logfiles)
+
+def debug(msg:str, logfiles=broadcast_logfiles) -> None:
+    print_console("debug", msg, [formating.blue])
+    print_logfile("debug", msg, logfiles)
+
+def trace(msg:str, logfiles=broadcast_logfiles) -> None:
+    print_console("trace", msg, [formating.white, formating.italic])
+    print_logfile("trace", msg, logfiles)
---
+++ src/file.py
@@ -0,0 +1,84 @@
+import json, pygame, os, copy, console
+from args import ext
+
+files = {}
+    
+def create(paths:list[str], data=None) -> None:
+    for path in paths:
+        files[f"{path}"] = data
+
+def close(paths:list[str]) -> None:
+    for path in paths:
+        try:
+            del files[f"{path}"]
+        except KeyError:
+            pass
+    
+def duplicate(old_path:str, new_paths:list[str]) -> None:
+    if f"{old_path}" not in files:
+        console.warn(f"Can't find the file '{old_path}' in memory.")
+        return
+    
+    for new_path in new_paths:
+        files[f"{new_path}"] = copy.deepcopy(files[f"{old_path}"])
+
+def read(paths:list[str]) -> None:
+    for path in paths:
+        try:
+            match "."+path.split(".")[-1]:
+                case ext.data:
+                    files[f"{path}"] = json.load(open(f"{path}", "r"))
+                case ext.image:
+                    files[f"{path}"] = pygame.image.load(f"{path}").convert_alpha()
+                case ext.text:
+                    files[f"{path}"] = open(f"{path}", "r").read()
+                case _:
+                    files[f"{path}"] = None
+                    console.warn(f"Can't open, unknown extension '{"."+path.split(".")[-1]}'.")
+            
+        except FileNotFoundError:
+            console.warn(f"File {path} not found.")
+
+
+def delete(paths:list[str]) -> None:
+    for path in paths:
+        try:
+            os.remove(f"{path}")
+        except FileNotFoundError:
+            pass
+
+
+def write(paths:list[str]) -> None:
+    for path in paths:
+        try:
+            match "."+path.split(".")[-1]:
+                case ext.data:
+                    json.dump(files[f"{path}"], read(f"{path}", "w"), indent=4)
+                case ext.image:
+                    pygame.image.save(files[f"{path}"], f"{path}")
+                case ext.text:
+                    read(f"{path}", "w").write(files[f"{path}"])
+                case _:
+                    console.warn(f"Can't write, unknown extension '{"."+path.split(".")[-1]}'.")
+            
+        except KeyError:
+            console.warn(f"Can't write {path}, not in memory.")
+
+
+def ask(paths:list[str]) -> list[object]:
+    read([path for path in paths if path not in files])
+    return [files.get(path, None) for path in paths]
+
+
+def directory(paths:list[str]) -> None:
+    for path in paths:
+        try:
+            os.makedirs(f"{path}")
+        except OSError:
+            pass
+
+def find(paths:list[str]) -> list[bool]:
+    result = []
+    for path in paths:
+        result.append(os.path.exists(f"{path}"))
+    return result
